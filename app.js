f548c27c0dc53f311c955aaef8bac3de2bd8ca9e
smays <smays@thunderhead.com>
2020-11-06 08:45:40 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var express = require("express"),
+mongoose = require("mongoose"),
+bodyParser = require("body-parser"),
+app = express()
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer");
+
+mongoose.connect("mongodb://localhost:27017/BlogApp", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//ROUTES
+
+app.get("/", function(req,res){
+	res.redirect("blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {blogs : blogs});
+		}
+	});
+});
+
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+
+// CREATE
+app.post("/blogs", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new")
+		} else {
+			res.redirect("/blogs");
+		}
+	})
+});
+
+// SHOW
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+// EDIT
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//UPDATE
+app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+//DELETE
+app.delete("/blogs/:id", function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs")
+		}
+	});
+});
+
+app.listen(3002, function(){
+	console.log("server is running");
+});
